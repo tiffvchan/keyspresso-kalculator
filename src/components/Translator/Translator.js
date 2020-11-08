15feee977249396e6311461a2d5e87157e0a42f7
Tiffany <tiffanyvchan@gmail.com>
2020-11-08 03:40:20 -0500

    fixed error handling, added toastify pkg, added copy to clipboard functionality

--- src/components/Translator/Translator.js
+++ src/components/Translator/Translator.js
@@ -1,34 +1,48 @@
 import React, {useState} from "react";
 import "./Translator.scss";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { translate, stringSplitter } from "../../utils/";
 
 // Splits the string arr further by qty, and reattaches.
 const splitAndTranslate = (arr, cb) => {
-  return arr
+  const errorArr = []
+
+  const translatedString = arr
     .map((item) => {
       let trimmed = item.trim();
       let qty = trimmed.substr(0, trimmed.indexOf(" "));
       let product = trimmed.substr(trimmed.indexOf(" ") + 1)
       let productTranslated = translate(product);
       
+      // compile error items
       if(productTranslated === product) {
-        cb(`Translation for '${product}' was not found`)
+        errorArr.push(product)
       }
       
       let tempArr = [productTranslated === product ? product : productTranslated, qty];
       return tempArr.join(" x ");
     })
     .join("; ");
+  
+  cb(errorArr)
+
+  return translatedString;
 };
+
+const autoCopyToClipboard = () => {
+  var copyTextarea = document.getElementById("output-text");
+  copyTextarea.select(); //select the text area
+  document.execCommand("copy"); //copy to clipboard
+  toast("Copied to Clipboard!")
+}
 
 const Translator = () => {
   const [returnValue, updateReturnValue] = useState("");
-  const [error, setError] = useState(false)
-  const errorCallback = (value) => {
-    setError(value)
-  }
-  const handleSubmit = (e) => {
+  const [error, setError] = useState([])
+  const errorCallback = (value) => setError(value)
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Split the string by ';'
     let splitValues = stringSplitter(e.target.input.value, ";");
@@ -37,7 +51,9 @@ const Translator = () => {
       (text) => !text == "" && !text.includes("Route")
     );
 
-    return updateReturnValue(splitAndTranslate(splitValues, errorCallback));
+    await updateReturnValue(splitAndTranslate(splitValues, errorCallback));
+    autoCopyToClipboard()
+
   };
 
   return (
@@ -45,13 +61,34 @@ const Translator = () => {
       <form className="Translator__form" onSubmit={handleSubmit}>
         <textarea
           className="Translator__input"
-          // onChange={handleChange}
           name="input"
         ></textarea>
         <button className="Translator__button">Enter</button>
       </form>
-      <div>{returnValue}</div>
-      <div>{error}</div>
+      <div>
+      <textarea
+          id="output-text"
+          className="Translator__input"
+          name="output"
+          value={returnValue}
+        ></textarea>
+      </div>
+      {!!error.length && 
+      <div className="Translator__error">
+        Translation not found: {error.join(", ")}
+      </div>
+      }
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 };
