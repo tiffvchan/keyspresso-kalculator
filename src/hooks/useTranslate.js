7d0b562cb5ec500aec0c261c4d57b6226da7a4c2
Hugo <hugo.fok94@gmail.com>
2020-11-08 00:15:31 -0500

    wip

--- src/hooks/useTranslate.js
+++ src/hooks/useTranslate.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+// Translation hook
 const useTranslate = (string) => {
   const { t, i18n } = useTranslation();
   return t(string);
